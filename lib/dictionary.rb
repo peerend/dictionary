40f6d552146cbf28102a0ea07da353c9e38fd58d
Dylan Peerenboom & Tanner Steward <student@epicodus.com>
2014-07-30 15:52:51 -0700

    passes next three specs

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -2,10 +2,9 @@ class Term
 
 @@all_words = []
 
-  def initialize(word)
-
-    @definition = []
-    @word = word
+  def initialize(word_input, definition)
+    @definition = definition
+    @word_input = word_input
   end
 
   def Term.clear
@@ -13,22 +12,25 @@ class Term
   end
 
   def Term.words
-    @@all_words
+    words_array = []
+    @@all_words.each do |word|
+      words_array.push(word.word_input)
+    end
+    words_array
   end
 
   def save
     @@all_words << self
   end
 
-  def word
-    @word
+  def word_input
+    @word_input
   end
 
   def definition
     @definition
   end
-
   def add_definition(new_definition)
-    @definition << new_definition
+    @definition = @definition + ': ' +  new_definition
   end
 end
--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -2,8 +2,26 @@ require 'rspec'
 require 'dictionary'
 
 describe Term do
+  before do
+    Term.clear
+  end
   it 'is initalized with a word' do
-    test_word = Term.new('fish')
+    test_word = Term.new('fish', 'swim')
     expect(test_word).to be_an_instance_of Term
+  end
+  it 'lets you list a word' do
+    test_word = Term.new('scrub', 'alot')
+    expect(test_word.word_input).to eq 'scrub'
+  end
+  it 'lets you list multiple words' do
+    test_word = Term.new('scrub', 'alot').save
+    test_word2 = Term.new('wash', 'alot alot').save
+    expect(Term.words).to eq ["scrub", 'wash']
+  end
+  it 'lets you define a word twice' do
+    dog_word = Term.new('dog', 'four legged animal')
+    dog_word.add_definition('with hair')
+    expect(dog_word.definition).to eq 'four legged animal: with hair'
+
   end
 end
